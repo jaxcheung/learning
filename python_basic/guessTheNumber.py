f95e5506988a203d062f817827a494f56d8b8796
jaxcheung <pccheung0414@gmail.com>
2025-10-14 07:58:37 +0800

    Update new code

--- python_basic/guessTheNumber.py
+++ python_basic/guessTheNumber.py
@@ -1,1 +1,21 @@
-print("Hello from a new branch!")
+import random
+secret_number = random.randint(1,20)
+print("Number between 1 and 20.")
+
+#number_of_guess = 6
+
+for guesses_taken in range(1,7):
+    print('take 6 guess')
+    guess=int(input('>'))
+
+    if guess < secret_number:
+        print('too low')
+    elif guess > secret_number:
+        print('too large')
+    else:
+        break
+
+if guess == secret_number:
+    print('Good job! You got it in' + str(guesses_taken) + 'guesses!')
+else:
+    print('The number is %s', secret_number)
