fb81427115f19383a8fc3066829a1f414284acbe
jaxcheung <pccheung0414@gmail.com>
2025-10-14 08:18:01 +0800

    Enhance Guess the number code

--- python_basic/guessTheNumber.py
+++ python_basic/guessTheNumber.py
@@ -2,11 +2,17 @@ import random
 secret_number = random.randint(1,20)
 print("Number between 1 and 20.")
 
-#number_of_guess = 6
+number_of_guess = 6
 
-for guesses_taken in range(1,7):
-    print('take 6 guess')
-    guess=int(input('>'))
+for guesses_taken in range(1,number_of_guess + 1):
+    remaining_guesses = number_of_guess - guesses_taken
+    print(f'take guess, you have {remaining_guesses} guesses left')
+
+    try:
+        guess=int(input('>'))
+    except ValueError:
+        print('Please enter a valid number.')
+        continue
 
     if guess < secret_number:
         print('too low')
@@ -16,6 +22,6 @@ for guesses_taken in range(1,7):
         break
 
 if guess == secret_number:
-    print('Good job! You got it in' + str(guesses_taken) + 'guesses!')
+    print('Good job! You got it in ' + str(guesses_taken) + ' guesses!')
 else:
     print('The number is %s', secret_number)
